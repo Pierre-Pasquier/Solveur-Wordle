41f2799002dd66f6b83ce5136b0298f77c602fe0
Pierre <pierre.pasquier@telecomnancy.eu>
2022-05-13 18:03:27 +0200

    Ajout fonctions parcours arbre

--- Solveur/src/arbre.h
+++ Solveur/src/arbre.h
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 #include<assert.h>
 
 
@@ -19,12 +20,15 @@ struct _element_t{
 typedef struct _element_t element_t;
 
 struct _arbre_t {
+    int nbr_mots;
     element_t* racine;
 };
 
 typedef struct _arbre_t arbre_t;
 
 arbre_t* create_arbre_mots(int nbre_mots);
+
+bool is_in(char a,char *mot);
 
 void destroy_element(element_t* begin);
 
@@ -36,6 +40,10 @@ void print_rec_element(element_t* elem,unsigned char* prefix,int len);
 
 void print_arbre(arbre_t* abr);
 
+int paterne(char *mot_cherche,char *mot_donne);
 
+void mot_suivant(arbre_t *arbre,element_t * elem, char *prefixe,int num_mot_cherche,int num_mot_donne,int len_mot,int **matrice,int nb_mot);
+
+void parcours(arbre_t *arbre, int len_mot, int num_mot_cherche, int **matrice, element_t *position,char *mot_cherche,int nb_mot);
 
 #endif // ARBRE_H
