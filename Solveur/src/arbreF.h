109b5a6d199d6baca6e4cba997f7e6f92621e37d
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-05-21 14:15:04 +0000

    Replace arbreF.h

--- Solveur/src/arbreF.h
+++ Solveur/src/arbreF.h
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 struct _noeud_t{
     char * MotDuNoeud;
     _noeud_t *fils[];
@@ -12,3 +18,10 @@ struct _arbre_t{
 
 typedef struct _arbre_t arbre_t;
 
+noeud_t* create_noeud(int nbfils, int pattern, char MotDuNoeud);
+
+arbre_t* create_arbre();
+
+void destroy_noeud(noeud_t* noeud);
+
+void destroy_arbre(arbre_t* abr);
