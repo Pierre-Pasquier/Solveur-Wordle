df6fdafb976bdb122f2d9ff3949a5253a3a1bf2a
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-05-21 14:08:02 +0000

    road to test

---
+++ Solveur/src/arbreF.c
@@ -0,0 +1,43 @@
+#include "arbreF.h"
+
+
+noeud_t* create_noeud(int nbfils, int pattern, char MotDuNoeud){
+    noeud_t* noeud=malloc(sizeof(noeud_t));
+    for (int i=0;i<nbfils;i++){
+        noeud->fils[i]=NULL;
+    }
+    noeud->pattern=pattern;
+    noeud->MotDuNoeud=MotDuNoeud;
+    printf(noeud->nbfils,noeud->pattern,noeud->MotDuNoeud)
+    return noeud;
+}
+
+
+arbre_t* create_arbre(){
+    arbre_t* res=malloc(sizeof(arbre_t));
+    res->pere=NULL;
+    return res;
+}
+
+
+void destroy_noeud(noeud_t* noeud){
+    int a = 0;
+    for(int i=0;i<sizeof(noeud->fils),i++){
+        if(noeud->fils[i]!= NULL){
+            destroy_noeud(noeud->fils[i]);
+        }else{
+            free(noeud->fils[i]);
+        }
+    }
+    free(noeud->pattern);
+    free(noeud->MotDuNoeud);
+    free(noeud);
+}
+
+
+
+void destroy_arbre(arbre_t* abr){
+    noueud_t* noeud=abr->pere;
+    destroy_noeud(noeud);
+    free(abr);
+}
