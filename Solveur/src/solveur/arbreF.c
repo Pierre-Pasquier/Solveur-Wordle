74d1687c0019d3802a5b5fd804bf7df8d51cf635
Julien De Toffoli <julien.de-toffoli@telecomnancy.eu>
2022-05-25 19:23:02 +0200

    rajout des fichiers arbreF.c arbreF.h fait par Nathan et supprimé par Pierre afin de faire passer son push qui ne passait pas

---
+++ Solveur/src/solveur/arbreF.c
@@ -0,0 +1,55 @@
+#include "arbreF.h"
+
+noeud_t* create_noeud(int nbfils, int pattern, char *MotDuNoeud){
+    noeud_t* noeud=malloc(sizeof(noeud_t));
+    noeud_t** fils=malloc(sizeof(noeud_t*)*nbfils);
+    for (int i=0;i<nbfils;i++){
+        fils[i]=NULL;
+    }
+    noeud->nbfils=nbfils;
+    noeud->fils=fils;
+    noeud->pattern=pattern;
+    noeud->MotDuNoeud=MotDuNoeud;
+    return noeud;
+}
+
+arbre_t* create_arbre(){
+    arbre_t* res=malloc(sizeof(arbre_t));
+    res->pere=NULL;
+    return res;
+}
+
+
+void destroy_noeud(noeud_t* noeud){
+    if(noeud!=NULL){
+        for(int i=0;i<noeud->nbfils;i++){
+            if(noeud->fils[i]!= NULL){
+                destroy_noeud(noeud->fils[i]);
+                free(noeud->fils[i]);
+            }
+        }
+    free(noeud->fils);
+    free(noeud);
+    }
+}
+
+
+void destroy_arbre(arbre_t* abr){
+    noeud_t* noeud=abr->pere;
+    destroy_noeud(noeud);
+    free(abr);
+}
+
+
+int main(){
+
+    arbre_t* arb=create_arbre();
+    arb->pere=create_noeud(3,27,"chocolat");
+
+    printf("%s\n%d\n%d\n",arb->pere->MotDuNoeud,arb->pere->nbfils,arb->pere->pattern);
+
+    destroy_arbre(arb);
+
+    return(0);
+}
+
